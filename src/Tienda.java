dba937de5f38d6aee428d739bc8d6a9c1c60706f
Sofía Martínez <sofialem2006@gmail.com>
2025-05-01 17:00:13 -0500

    Create Tienda.java

---
+++ src/Tienda.java
@@ -0,0 +1,8 @@
+public class Tienda {
+    TiendaMascotas tienda = new TiendaMascotas();
+
+    especie1= new Especie("Cacatúa Blanca")
+    especie2= new Especie("Boa Constrictor")
+    especie3= new Especie("Tití Dorado")
+    especie4= new Especie("Guacamaya Roja")
+}
