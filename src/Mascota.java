e63aa1d63842f057a11c9ca50e663e6dc6ecdcfa
Sofía Martínez <sofialem2006@gmail.com>
2025-05-01 16:30:46 -0500

    Add files via upload

---
+++ src/Mascota.java
@@ -0,0 +1,11 @@
+public class Mascota {
+    private String nombre;
+    private int vidaPromedio;
+    private String claseAnimal;
+    private double precioVenta;
+    private int cantidadDisponible;
+    private int totalVendidas;
+
+}
+
+public Mas
