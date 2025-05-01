e9c6c2833c5e0e2d673699bc192c893105c93999
Sofía Martínez <sofialem2006@gmail.com>
2025-05-01 16:59:22 -0500

    Update and rename Mascota.java to Especie.java

---
+++ src/Especie.java
@@ -0,0 +1,37 @@
+public class Especie {
+    private String nombre;
+    private int vidaPromedio;
+    private String claseAnimal;
+    private double precioVenta;
+    private int cantidadDisponible;
+    private int totalVendidas;
+    public Especie(String nombre, int vidaPromedio, String claseAnimal, double precioVenta, int cantidadDisponible) {
+        this.nombre = nombre;
+        this.vidaPromedio = vidaPromedio;
+        this.claseAnimal = claseAnimal;
+        this.precioVenta = precioVenta;
+        this.cantidadDisponible = cantidadDisponible;
+        this.totalVendidas = 0;
+    }
+}
+public void mostrarInformacion() {
+    System.out.println("Nombre: " + nombre);
+    System.out.println("Vida promedio: " + vidaPromedio + " años");
+    System.out.println("Clase de animal: " + claseAnimal);
+    System.out.println("Precio de venta: $" + precioVenta);
+    System.out.println("Cantidad disponible: " + cantidadDisponible);
+    System.out.println("Cantidad total vendidas: " + totalVendidas);
+    System.out.println("--------------------------------");
+}
+
+public boolean vender(int cantidad) {
+    if (cantidad <= cantidadDisponible) {
+        cantidadDisponible -= cantidad;
+        totalVendidas += cantidad;
+        System.out.println("Venta realizada con éxito: " + cantidad + " " + nombre);
+        return true;
+    } else {
+        System.out.println("No hay suficientes " + nombre + " disponibles para la venta.");
+        return false;
+    }
+}
